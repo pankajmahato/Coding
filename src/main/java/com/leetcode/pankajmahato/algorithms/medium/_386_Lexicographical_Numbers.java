e82d72ae188eba18f5275d067a38d4f4cb892757
Pankaj Mahato <pankajmahato123@gmail.com>
2024-10-02 21:20:34 +0530

    386 Lexicographical Numbers

---
+++ src/main/java/com/leetcode/pankajmahato/algorithms/medium/_386_Lexicographical_Numbers.java
@@ -0,0 +1,65 @@
+/**********************************************************************************
+ *
+ * https://leetcode.com/problems/lexicographical-numbers/
+ *
+ * Given an integer n, return all the numbers in the range [1, n] sorted in lexicographical order.
+ *
+ * You must write an algorithm that runs in O(n) time and uses O(1) extra space. 
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: n = 13
+ * Output: [1,10,11,12,13,2,3,4,5,6,7,8,9]
+ * Example 2:
+ *
+ * Input: n = 2
+ * Output: [1,2]
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= n <= 5 * 104
+ *
+ **********************************************************************************/
+
+package com.leetcode.pankajmahato.algorithms.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _386_Lexicographical_Numbers {
+
+    public List<Integer> lexicalOrder(int n) {
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 1; i <= 9; i++) {
+            solve(i, n, result);
+        }
+        return result;
+    }
+
+    private void solve(int curr, int n, List<Integer> result) {
+        if (curr > n) {
+            return;
+        }
+        result.add(curr);
+
+        for (int nextDigit = 0; nextDigit <= 9; nextDigit++) {
+            int nextNum = curr * 10 + nextDigit;
+
+            if (nextNum > n) {
+                return;
+            }
+
+            solve(nextNum, n, result);  // Recursive call
+        }
+    }
+
+    public static void main(String[] args) {
+        _386_Lexicographical_Numbers1 obj = new _386_Lexicographical_Numbers1();
+        System.out.println(obj.lexicalOrder(13));
+    }
+}
