8db4b58e6627f424e9e43c2fc5c806f9a1591126
Pankaj Mahato <pankajmahato123@gmail.com>
2024-11-06 16:08:06 +0530

    1926 Nearest Exit from Entrance in Maze

---
+++ src/main/java/com/leetcode/pankajmahato/algorithms/medium/graph/_1926_Nearest_Exit_from_Entrance_in_Maze.java
@@ -0,0 +1,111 @@
+/**********************************************************************************
+ *
+ * https://leetcode.com/problems/nearest-exit-from-entrance-in-maze/
+ *
+ * You are given an m x n matrix maze (0-indexed) with empty cells (represented as '.') and walls (represented as '+'). You are also given the entrance of the maze, where entrance = [entrancerow, entrancecol] denotes the row and column of the cell you are initially standing at.
+ *
+ * In one step, you can move one cell up, down, left, or right. You cannot step into a cell with a wall, and you cannot step outside the maze. Your goal is to find the nearest exit from the entrance. An exit is defined as an empty cell that is at the border of the maze. The entrance does not count as an exit.
+ *
+ * Return the number of steps in the shortest path from the entrance to the nearest exit, or -1 if no such path exists.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: maze = [["+","+",".","+"],[".",".",".","+"],["+","+","+","."]], entrance = [1,2]
+ * Output: 1
+ * Explanation: There are 3 exits in this maze at [1,0], [0,2], and [2,3].
+ * Initially, you are at the entrance cell [1,2].
+ * - You can reach [1,0] by moving 2 steps left.
+ * - You can reach [0,2] by moving 1 step up.
+ * It is impossible to reach [2,3] from the entrance.
+ * Thus, the nearest exit is [0,2], which is 1 step away.
+ * Example 2:
+ *
+ *
+ * Input: maze = [["+","+","+"],[".",".","."],["+","+","+"]], entrance = [1,0]
+ * Output: 2
+ * Explanation: There is 1 exit in this maze at [1,2].
+ * [1,0] does not count as an exit since it is the entrance cell.
+ * Initially, you are at the entrance cell [1,0].
+ * - You can reach [1,2] by moving 2 steps right.
+ * Thus, the nearest exit is [1,2], which is 2 steps away.
+ * Example 3:
+ *
+ *
+ * Input: maze = [[".","+"]], entrance = [0,0]
+ * Output: -1
+ * Explanation: There are no exits in this maze.
+ *
+ *
+ * Constraints:
+ *
+ * maze.length == m
+ * maze[i].length == n
+ * 1 <= m, n <= 100
+ * maze[i][j] is either '.' or '+'.
+ * entrance.length == 2
+ * 0 <= entrancerow < m
+ * 0 <= entrancecol < n
+ * entrance will always be an empty cell.
+ *
+ **********************************************************************************/
+
+package com.leetcode.pankajmahato.algorithms.medium.graph;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class _1926_Nearest_Exit_from_Entrance_in_Maze {
+
+    public int nearestExit(char[][] maze, int[] entrance) {
+
+        int m = maze.length;
+        int n = maze[0].length;
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{entrance[0], entrance[1]});
+        maze[entrance[0]][entrance[1]] = '+';
+
+        int[][] directions = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        int steps = 0;
+
+        while (!queue.isEmpty()) {
+
+            int size = queue.size();
+            while (size-- > 0) {
+                int[] val = queue.remove();
+                int i = val[0];
+                int j = val[1];
+
+                if (isExit(i, j, m, n) && !(i == entrance[0] && j == entrance[1])) {
+                    return steps;
+                }
+                for (int[] dir : directions) {
+                    int x = i + dir[0];
+                    int y = j + dir[1];
+
+                    if (canMove(x, y, m, n, maze)) {
+                        queue.add(new int[]{x, y});
+                        maze[x][y] = '+';
+                    }
+                }
+            }
+            steps++;
+        }
+
+        return -1;
+    }
+
+    private boolean canMove(int i, int j, int m, int n, char[][] maze) {
+        return i >= 0 && i < m && j >= 0 && j < n && maze[i][j] == '.';
+    }
+
+    private boolean isExit(int i, int j, int m, int n) {
+        return (i == 0 || j == 0 || i == m - 1 || j == n - 1);
+    }
+
+}
