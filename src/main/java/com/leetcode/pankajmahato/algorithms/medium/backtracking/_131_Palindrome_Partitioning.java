073d408b5f567433642ac6c2d27b611576966b90
Pankaj Mahato <pankajmahato123@gmail.com>
2024-12-06 21:35:37 +0530

    131 Palindrome Partitioning

--- src/main/java/com/leetcode/pankajmahato/algorithms/medium/backtracking/_131_Palindrome_Partitioning.java
+++ src/main/java/com/leetcode/pankajmahato/algorithms/medium/backtracking/_131_Palindrome_Partitioning.java
@@ -34,39 +34,88 @@ import java.util.List;
 
 public class _131_Palindrome_Partitioning {
 
+//    public List<List<String>> partition(String s) {
+//
+//        // Backtracking
+//        List<String> curr = new ArrayList<>();
+//        List<List<String>> result = new ArrayList<>();
+//
+//        backtrack(s, 0, curr, result);
+//        return result;
+//    }
+//
+//    private void backtrack(String s, int idx, List<String> curr, List<List<String>> result) {
+//
+//        if (idx == s.length()) {
+//            result.add(new ArrayList(curr));
+//        }
+//
+//        for (int i = idx; i < s.length(); i++) {
+//            if (isPalindrome(s, idx, i)) {
+//                curr.add(s.substring(idx, i + 1));
+//                backtrack(s, i + 1, curr, result);
+//                curr.remove(curr.size() - 1);
+//            }
+//        }
+//    }
+//
+//    private boolean isPalindrome(String s, int i, int j) {
+//        while (i <= j) {
+//            if (s.charAt(i) != s.charAt(j)) {
+//                return false;
+//            }
+//            i++;
+//            j--;
+//        }
+//
+//        return true;
+//    }
+
     public List<List<String>> partition(String s) {
+
+        // DP with bottom up
+        int n = s.length();
+
+        // dp[i][j] = is substring [i....j] palindrome
+        boolean[][] dp = new boolean[n][n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i + len - 1 < n; i++) {
+
+                int j = i + len - 1;
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (len == 2) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+            }
+        }
 
         List<String> curr = new ArrayList<>();
         List<List<String>> result = new ArrayList<>();
 
-        backtrack(s, 0, curr, result);
+        backtrack(s, 0, curr, result, dp);
         return result;
     }
 
-    private void backtrack(String s, int idx, List<String> curr, List<List<String>> result) {
+    private void backtrack(String s, int idx, List<String> curr, List<List<String>> result, boolean[][] dp) {
 
         if (idx == s.length()) {
             result.add(new ArrayList(curr));
         }
 
         for (int i = idx; i < s.length(); i++) {
-            if (isPalindrome(s, idx, i)) {
+            if (dp[idx][i] == true) {
                 curr.add(s.substring(idx, i + 1));
-                backtrack(s, i + 1, curr, result);
+                backtrack(s, i + 1, curr, result, dp);
                 curr.remove(curr.size() - 1);
             }
         }
-    }
-
-    private boolean isPalindrome(String s, int i, int j) {
-        while (i <= j) {
-            if (s.charAt(i) != s.charAt(j)) {
-                return false;
-            }
-            i++;
-            j--;
-        }
-
-        return true;
     }
 }
