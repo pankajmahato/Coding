ebda3ecd1e891a7275f4be2e29827c546ac8fbea
Pankaj Mahato <pankajmahato123@gmail.com>
2024-12-13 22:02:17 +0530

    1512 Number of Good Pairs

---
+++ src/main/java/com/leetcode/pankajmahato/algorithms/easy/hashmap/_1512_Number_of_Good_Pairs.java
@@ -0,0 +1,54 @@
+/**********************************************************************************
+ *
+ * https://leetcode.com/problems/number-of-good-pairs/
+ *
+ * Given an array of integers nums, return the number of good pairs.
+ *
+ * A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,2,3,1,1,3]
+ * Output: 4
+ * Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+ * Example 2:
+ *
+ * Input: nums = [1,1,1,1]
+ * Output: 6
+ * Explanation: Each pair in the array are good.
+ * Example 3:
+ *
+ * Input: nums = [1,2,3]
+ * Output: 0
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 100
+ * 1 <= nums[i] <= 100
+ *
+ **********************************************************************************/
+
+package com.leetcode.pankajmahato.algorithms.easy.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _1512_Number_of_Good_Pairs {
+
+    public int numIdenticalPairs(int[] nums) {
+
+        // Key = num; Value = count
+        Map<Integer, Integer> numCountMap = new HashMap<>();
+
+        int result = 0;
+        for (int num : nums) {
+            result += numCountMap.getOrDefault(num, 0);
+            numCountMap.put(num, numCountMap.getOrDefault(num, 0) + 1);
+        }
+
+        return result;
+    }
+}
