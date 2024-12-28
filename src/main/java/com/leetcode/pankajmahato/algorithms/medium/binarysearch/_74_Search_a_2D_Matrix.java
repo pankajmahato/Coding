2b5d67c32bd94da718a4a4ec462c0b8e674d9d35
Pankaj Mahato <pankajmahato123@gmail.com>
2024-12-28 19:01:42 +0530

    74 Search a 2D Matrix

--- src/main/java/com/leetcode/pankajmahato/algorithms/medium/binarysearch/_74_Search_a_2D_Matrix.java
+++ src/main/java/com/leetcode/pankajmahato/algorithms/medium/binarysearch/_74_Search_a_2D_Matrix.java
@@ -2,10 +2,14 @@
  *
  * https://leetcode.com/problems/search-a-2d-matrix/
  *
- * Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+ * You are given an m x n integer matrix matrix with the following two properties:
  *
- * Integers in each row are sorted from left to right.
+ * Each row is sorted in non-decreasing order.
  * The first integer of each row is greater than the last integer of the previous row.
+ * Given an integer target, return true if target is in matrix or false otherwise.
+ *
+ * You must write a solution in O(log(m * n)) time complexity.
+ *
  *
  *
  * Example 1:
@@ -32,40 +36,64 @@
 package com.leetcode.pankajmahato.algorithms.medium.binarysearch;
 
 public class _74_Search_a_2D_Matrix {
+//    public boolean searchMatrix(int[][] matrix, int target) {
+//
+//        int n = matrix.length;
+//        int m = matrix[0].length;
+//        int row = -1;
+//        int low = 0;
+//        int high = n - 1;
+//
+//
+//        while (low <= high) {
+//            int mid = low + (high - low) / 2;
+//            if (target >= matrix[mid][0] && target <= matrix[mid][m - 1]) {
+//                row = mid;
+//                break;
+//            } else if (target < matrix[mid][0]) {
+//                high = mid - 1;
+//            } else {
+//                low = mid + 1;
+//            }
+//        }
+//        if (row < 0 || row >= n) {
+//            return false;
+//        }
+//
+//        low = 0;
+//        high = m - 1;
+//        while (low <= high) {
+//            int mid = low + (high - low) / 2;
+//            if (target == matrix[row][mid]) {
+//                return true;
+//            } else if (target < matrix[row][mid]) {
+//                high = mid - 1;
+//            } else {
+//                low = mid + 1;
+//            }
+//        }
+//
+//        return false;
+//    }
+
     public boolean searchMatrix(int[][] matrix, int target) {
 
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int row = -1;
-        int low = 0;
-        int high = n - 1;
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int l = 0;
+        int r = row * col - 1;
 
+        while (l <= r) {
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (target >= matrix[mid][0] && target <= matrix[mid][m - 1]) {
-                row = mid;
-                break;
-            } else if (target < matrix[mid][0]) {
-                high = mid - 1;
+            int mid = l + (r - l) / 2;
+            int i = mid / col;
+            int j = mid % col;
+            if (matrix[i][j] < target) {
+                l = mid + 1;
+            } else if (matrix[i][j] > target) {
+                r = mid - 1;
             } else {
-                low = mid + 1;
-            }
-        }
-        if (row < 0 || row >= n) {
-            return false;
-        }
-
-        low = 0;
-        high = m - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (target == matrix[row][mid]) {
                 return true;
-            } else if (target < matrix[row][mid]) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
             }
         }
 
