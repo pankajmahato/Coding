ce8e669909129e945bb894759fbcd2c3b0d52e2b
Pankaj Mahato <pankajmahato123@gmail.com>
2024-12-30 16:56:11 +0530

    1539 Kth Missing Positive Number

---
+++ src/main/java/com/leetcode/pankajmahato/algorithms/easy/binarysearch/_1539_Kth_Missing_Positive_Number.java
@@ -0,0 +1,65 @@
+/**********************************************************************************
+ *
+ * https://leetcode.com/problems/kth-missing-positive-number/
+ *
+ * Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+ *
+ * Return the kth positive integer that is missing from this array.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: arr = [2,3,4,7,11], k = 5
+ * Output: 9
+ * Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
+ * Example 2:
+ *
+ * Input: arr = [1,2,3,4], k = 2
+ * Output: 6
+ * Explanation: The missing positive integers are [5,6,7,...]. The 2nd missing positive integer is 6.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= arr.length <= 1000
+ * 1 <= arr[i] <= 1000
+ * 1 <= k <= 1000
+ * arr[i] < arr[j] for 1 <= i < j <= arr.length
+ *
+ *
+ * Follow up:
+ *
+ * Could you solve this problem in less than O(n) complexity?
+ *
+ **********************************************************************************/
+
+package com.leetcode.pankajmahato.algorithms.easy.binarysearch;
+
+public class _1539_Kth_Missing_Positive_Number {
+
+    public int findKthPositive(int[] arr, int k) {
+
+        int n = arr.length;
+        int l = 0;
+        int r = n - 1;
+
+        while (l <= r) {
+
+            int mid = l + (r - l) / 2;
+            int noMissingTillMid = arr[mid] - (mid + 1);
+
+            if (noMissingTillMid < k) {
+                // move right
+                l = mid + 1;
+            } else {
+                // move left
+                r = mid - 1;
+            }
+        }
+
+        // Missing number if l-th index + k
+
+        return l + k;
+    }
+}
