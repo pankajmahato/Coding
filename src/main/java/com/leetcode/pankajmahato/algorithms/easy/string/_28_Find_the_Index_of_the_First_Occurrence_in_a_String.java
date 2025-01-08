a4ab279df1bfad8b81c6ce8eb9d776957aada5f4
Pankaj Mahato <pankajmahato123@gmail.com>
2025-01-09 00:34:01 +0530

    28 Find the Index of the First Occurrence in a String

--- src/main/java/com/leetcode/pankajmahato/algorithms/easy/string/_28_Find_the_Index_of_the_First_Occurrence_in_a_String.java
+++ src/main/java/com/leetcode/pankajmahato/algorithms/easy/string/_28_Find_the_Index_of_the_First_Occurrence_in_a_String.java
@@ -30,24 +30,83 @@ package com.leetcode.pankajmahato.algorithms.easy.string;
 
 public class _28_Find_the_Index_of_the_First_Occurrence_in_a_String {
 
+//    public int strStr(String haystack, String needle) {
+//
+//        int m = haystack.length();
+//        int n = needle.length();
+//
+//        for (int i = 0; i <= m - n; i++) {
+//            int j = 0;
+//            for (; j < n; j++) {
+//                if (haystack.charAt(i + j) != needle.charAt(j)) {
+//                    break;
+//                }
+//            }
+//
+//            if (j == n) {
+//                return i;
+//            }
+//        }
+//
+//        return -1;
+//    }
+
     public int strStr(String haystack, String needle) {
 
+        // KMP
         int m = haystack.length();
         int n = needle.length();
 
-        for (int i = 0; i <= m - n; i++) {
-            int j = 0;
-            for (; j < n; j++) {
-                if (haystack.charAt(i + j) != needle.charAt(j)) {
-                    break;
-                }
+        // LPS[i] = Length of longest equal prefix and suffix ending at i-th index
+        int[] LPS = new int[n];
+        computeLPS(needle, n, LPS);
+
+        int i = 0;
+        int j = 0;
+
+        while (i < m) {
+
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
             }
 
             if (j == n) {
-                return i;
+                return i - j;
+            } else if (i < m && haystack.charAt(i) != needle.charAt(j)) {
+                if (j != 0) {
+                    j = LPS[j - 1];
+                } else {
+                    i++;
+                }
             }
         }
 
         return -1;
+    }
+
+    private void computeLPS(String needle, int n, int[] LPS) {
+
+        // Length of the previous longest prefix suffix
+        int len = 0;
+
+        LPS[0] = 0;
+
+        int i = 1;
+        while (i < n) {
+
+            if (needle.charAt(i) == needle.charAt(len)) {
+                len++;
+                LPS[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = LPS[len - 1];
+                } else {
+                    LPS[i] = 0;
+                    i++;
+                }
+            }
+        }
     }
 }
