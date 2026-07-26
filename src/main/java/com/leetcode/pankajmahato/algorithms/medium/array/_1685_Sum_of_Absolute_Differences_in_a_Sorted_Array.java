7a91d95b05ee3b7a574142d64445b8d11d3a2659
Pankaj Mahato <pankajmahato123@gmail.com>
2026-07-26 13:11:29 +0530

    1685 Sum of Absolute Differences in a Sorted Array

---
+++ src/main/java/com/leetcode/pankajmahato/algorithms/medium/array/_1685_Sum_of_Absolute_Differences_in_a_Sorted_Array.java
@@ -0,0 +1,61 @@
+/**********************************************************************************
+ *
+ * https://leetcode.com/problems/sum-of-absolute-differences-in-a-sorted-array/
+ *
+ * You are given an integer array nums sorted in non-decreasing order.
+ *
+ * Build and return an integer array result with the same length as nums such that result[i] is equal to the summation of absolute differences between nums[i] and all the other elements in the array.
+ *
+ * In other words, result[i] is equal to sum(|nums[i]-nums[j]|) where 0 <= j < nums.length and j != i (0-indexed).
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [2,3,5]
+ * Output: [4,3,5]
+ * Explanation: Assuming the arrays are 0-indexed, then
+ * result[0] = |2-2| + |2-3| + |2-5| = 0 + 1 + 3 = 4,
+ * result[1] = |3-2| + |3-3| + |3-5| = 1 + 0 + 2 = 3,
+ * result[2] = |5-2| + |5-3| + |5-5| = 3 + 2 + 0 = 5.
+ * Example 2:
+ *
+ * Input: nums = [1,4,6,8,10]
+ * Output: [24,15,13,15,21]
+ *
+ *
+ * Constraints:
+ *
+ * 2 <= nums.length <= 105
+ * 1 <= nums[i] <= nums[i + 1] <= 104
+ *
+ **********************************************************************************/
+
+package com.leetcode.pankajmahato.algorithms.medium.array;
+
+public class _1685_Sum_of_Absolute_Differences_in_a_Sorted_Array {
+
+    public int[] getSumAbsoluteDifferences(int[] nums) {
+
+        int n = nums.length;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum = sum + nums[i];
+        }
+
+        int prefixSum = 0;
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+
+            int left = prefixSum;
+            int right = sum - prefixSum - nums[i];
+
+            // result[i] = (nums[i] * i) - (0 to i-1 sum) + (i+1 to n-1 sum) - (nums[i] * (n-i-1))
+            result[i] = (nums[i] * i - left) + (right - (nums[i] * (n - i - 1)));
+            prefixSum = prefixSum + nums[i];
+        }
+
+        return result;
+    }
+}
