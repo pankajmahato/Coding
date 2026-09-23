aa4b7612df9b6e5c1b3b80f36672b3f92616b313
Pankaj Mahato <pankajmahato123@gmail.com>
2026-09-23 21:19:38 +0530

    165 Compare Version Numbers

---
+++ src/main/java/com/leetcode/pankajmahato/algorithms/medium/string/_165_Compare_Version_Numbers.java
@@ -0,0 +1,85 @@
+/**********************************************************************************
+ *
+ * https://leetcode.com/problems/compare-version-numbers/
+ *
+ * Given two version strings, version1 and version2, compare them. A version string consists of revisions separated by dots '.'. The value of the revision is its integer conversion ignoring leading zeros.
+ *
+ * To compare version strings, compare their revision values in left-to-right order. If one of the version strings has fewer revisions, treat the missing revision values as 0.
+ *
+ * Return the following:
+ *
+ * If version1 < version2, return -1.
+ * If version1 > version2, return 1.
+ * Otherwise, return 0.
+ *
+ *
+ * Example 1:
+ *
+ * Input: version1 = "1.2", version2 = "1.10"
+ *
+ * Output: -1
+ *
+ * Explanation:
+ *
+ * version1's second revision is "2" and version2's second revision is "10": 2 < 10, so version1 < version2.
+ *
+ * Example 2:
+ *
+ * Input: version1 = "1.01", version2 = "1.001"
+ *
+ * Output: 0
+ *
+ * Explanation:
+ *
+ * Ignoring leading zeroes, both "01" and "001" represent the same integer "1".
+ *
+ * Example 3:
+ *
+ * Input: version1 = "1.0", version2 = "1.0.0.0"
+ *
+ * Output: 0
+ *
+ * Explanation:
+ *
+ * version1 has less revisions, which means every missing revision are treated as "0".
+ *
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= version1.length, version2.length <= 500
+ * version1 and version2 only contain digits and '.'.
+ * version1 and version2 are valid version numbers.
+ * All the given revisions in version1 and version2 can be stored in a 32-bit integer.
+ *
+ **********************************************************************************/
+
+package com.leetcode.pankajmahato.algorithms.medium.string;
+
+public class _165_Compare_Version_Numbers {
+
+    public int compareVersion(String version1, String version2) {
+
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+
+        int n1 = v1.length;
+        int n2 = v2.length;
+
+        int len = Math.max(n1, n2);
+        for (int i = 0; i < len; i++) {
+
+            int num1 = i < n1 ? Integer.valueOf(v1[i]) : 0;
+            int num2 = i < n2 ? Integer.valueOf(v2[i]) : 0;
+
+            if (num1 < num2) {
+                return -1;
+            }
+            if (num1 > num2) {
+                return 1;
+            }
+        }
+
+        return 0;
+    }
+}
